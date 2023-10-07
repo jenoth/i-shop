c9657a955cd3c7a396368617e86d37d6defd71c9
Jenoth Ponnampalam <jenothp@gmail.com>
2023-10-07 21:42:20 +0530

    Overridden save method of cart model to allow only one active cart for a customer in the database

--- onlineishop/carts/models.py
+++ onlineishop/carts/models.py
@@ -18,3 +18,10 @@ class Cart(models.Model):
 
     def __str__(self):
         return f"{self.id}, Shopping cart of {self.customer}"
+
+    def save(self, *args, **kwargs):
+        """Overriding the existing save method which is triggered when create or update the record."""
+        if carts := self.is_active and Cart.objects.filter(customer=self.customer, is_active=True):
+            if (carts.count() > 1) or (self.pk is None) or (self.id != carts[0].id):
+                raise ValueError("Customer has active cart(s) which is a db integrity error according to our design")
+        super().save(*args, **kwargs)
